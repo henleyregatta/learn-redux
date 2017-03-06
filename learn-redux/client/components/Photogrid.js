22044495bf8331b219cf1a2abee17a1a35ad38d8
henleyregatta <henley@outlook.fr>
2017-03-06 15:46:11 +0100

    creation des components

---
+++ learn-redux/client/components/Photogrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Photogrid extends React.Component {
+	render () {
+		return (
+			<div className="photo-grid">
+				Photogrid component test
+
+			</div>		
+
+
+		)
+	}
+} 
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Single extends React.Component {
+	render () {
+		return (
+			<div className="single-photo">
+				Single photo component test
+
+			</div>		
+
+
+		)
+	}
+} 
